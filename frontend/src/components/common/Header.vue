85cfae10c02dc1d4f7c9d32317491bf14a6aca2d
PARKJUNYOUNG <rkt1313@naver.com>
2026-02-03 15:51:20 +0900

    feat: 헤더 프로필 메뉴 추가

--- frontend/src/components/common/Header.vue
+++ frontend/src/components/common/Header.vue
@@ -8,9 +8,51 @@
         <div class="time_text">{{}}</div>
         <div class="user_info">
           {{ logUserId }}님
-          <button>
-            <img src="/assets/icons/arrow_down.svg" alt="" />
+          <button @click="toggleDropdown">
+            <img src="/assets/icons/arrow_down.svg" alt="arrow_icon" />
           </button>
+          <!-- 드롭다운 -->
+          <div class="dropdown_menu" v-if="isDropdownOpen">
+            <ul class="menu_list">
+              <li>
+                <router-link to="/user-info/profile">
+                  <img src="/assets/icons/profile.svg" alt="profile_icon">
+                  프로필
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/user-info/password-change">
+                  <img src="/assets/icons/password.svg" alt="password_icon">
+                  비밀번호 변경
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/user-info/notice">
+                  <img src="/assets/icons/notice.svg" alt="notice_icon">
+                  공지사항
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/user-info/inquiry">
+                  <img src="/assets/icons/inquiry.svg" alt="inquiry_icon">
+                  문의하기
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/user-info/credit">
+                  <img src="/assets/icons/credit.svg" alt="credit_icon">
+                  크레딧 관리
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/user-info/info">
+                  <img src="/assets/icons/info.svg" alt="info_icon">
+                  정보
+                </router-link>
+              </li>
+            </ul>
+          </div>
+
         </div>
         <button class="log_out" @click="handleLogout">로그아웃</button>
       </div>
@@ -18,11 +60,26 @@
   </header>
 </template>
 <script setup>
-import { onMounted, ref } from 'vue';
-import {useRouter} from 'vue-router';
+import { onMounted, ref ,onUnmounted} from 'vue';
+import { useRouter } from 'vue-router';
 import { authAPI} from '@/api/auth';
 
 const router = useRouter();
+const isDropdownOpen = ref(false);
+
+const toggleDropdown = () => {
+  isDropdownOpen.value = !isDropdownOpen.value;
+};
+
+// 외부 클릭 시 닫기
+const closeDropdown = (e) => {
+  if (!e.target.closest('.user_info')) {
+    isDropdownOpen.value = false;
+  }
+}
+
+
+
 
 
 const demoId = '테스트';
@@ -46,7 +103,15 @@ const handleLogout = async () => {
 
 onMounted(() => {
   setData();
+  document.addEventListener('click', closeDropdown);
+
 });
+
+onUnmounted(()=> {
+    document.removeEventListener('click', closeDropdown);
+})
+
+
 
 const setData = () => {
   if (isConnect.value) {
@@ -83,6 +148,29 @@ const setData = () => {
       .user_info {
         display: flex;
         gap: 8px;
+        position:relative;
+        height:100%;
+      }
+      .dropdown_menu{
+        position:absolute;
+        top:calc(100% + 10px);
+        // left:50%;
+        right:0;
+        // transform:translateX(-50%);
+        padding:12px;
+        max-width:236px;
+        width:236px;
+        background:$sub-color;
+        border-radius:$radius-md;
+        z-index:10;
+        li a{
+          display:flex;
+          align-items:center;
+          padding: 8px 24px;
+          @include font-13-medium;
+          gap:8px;
+        }
+
       }
     }
     .log_out {
