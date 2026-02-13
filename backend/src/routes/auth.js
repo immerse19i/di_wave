f87397d9b07e495b11114a3b2f446c88085dc024
PARKJUNYOUNG <rkt1313@naver.com>
2026-02-13 14:43:08 +0900

    feat: 회원가입 추가

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { verifyToken , isAdmin } = require('../middlewares/auth');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'uploads/business/'),
+  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
+});
+const uploadBusiness = multer({ storage });
 
 
 // 로그인
@@ -19,5 +25,13 @@ router.put('/password', verifyToken, authController.changePassword);
 
 // 관리자 전용: 계정 잠금 해제
 router.patch('/unlock/:userId', verifyToken, isAdmin, authController.unlockAccount);
+
+
+// 회원가입로직
+router.post('/check-id', authController.checkId);
+router.post('/send-code', authController.sendCode);
+router.post('/verify-code', authController.verifyCode);
+router.post('/register', uploadBusiness.single('businessLicense'), authController.register);
+
 
 module.exports = router;
