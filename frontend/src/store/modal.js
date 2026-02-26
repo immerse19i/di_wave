640b496da622787d0a7366cbfb7dbff26d5789ff
PARKJUNYOUNG <rkt1313@naver.com>
2026-02-26 18:09:24 +0900

    feat: 로딩 스피너 추가

--- frontend/src/store/modal.js
+++ frontend/src/store/modal.js
@@ -5,16 +5,19 @@ export const useModalStore = defineStore('modal', () => {
   const isOpen = ref(false);
   const role = ref('');
   const type = ref('');
+  const isLoading = ref(false);
+
   const open = (modalType, modalRole) => {
     role.value = modalRole;
     type.value = modalType;
     isOpen.value = true;
   };
   const close = () => {
+    if (isLoading.value) return;
     isOpen.value = false;
     type.value = '';
     role.value = '';
   };
 
-  return { isOpen, type, role, open, close };
+  return { isOpen, type, role, isLoading, open, close };
 });
