1be9797166b46f881ff74e8358781a035f85e369
PARKJUNYOUNG <rkt1313@naver.com>
2026-03-25 14:19:04 +0900

    style: 가입계정목록 스타일 수정

--- frontend/src/views/admin/AccountList.vue
+++ frontend/src/views/admin/AccountList.vue
@@ -414,8 +414,8 @@ onMounted(() => fetchList());
     }
 
     &.active {
-      background: $main-color;
-      border-color: $main-color;
+      background: $sub-color;
+      border-color: $sub-color;
       color: $white;
     }
   }
@@ -487,12 +487,7 @@ onMounted(() => fetchList());
     padding: 8px 16px;
     border-radius: $radius-sm;
     background: $bg-op;
-    border: 1px solid;
-    border-image-source: linear-gradient(
-      90deg,
-      rgba(255, 255, 255, 0.09) 0%,
-      rgba(255, 255, 255, 0.06) 100%
-    );
+    border: 1px solid $dark-line-gray;
 
     input {
       width: 100%;
@@ -507,7 +502,7 @@ onMounted(() => fetchList());
 
     &:has(input:focus) {
       border-image-source: none;
-      border-color: $main-color;
+      border-color: $sub-color-2;
     }
   }
 
@@ -524,16 +519,21 @@ onMounted(() => fetchList());
 
 // 테이블
 .content_list {
+  padding: 12px 16px;
+  background: $table-bg;
+  border-radius: 12px;
   .table-top {
     display: flex;
     justify-content: flex-end;
-    align-items: center;
-    gap: 16px;
+    align-items: flex-end;
+    flex-direction: column;
+    gap: 8px;
     margin-bottom: 8px;
   }
 
   .btn-add {
-    padding: 8px 20px;
+    padding: 7px 20px;
+    min-width: 100px;
     background: $main-gad;
     color: $white;
     border-radius: $radius-sm;
@@ -542,7 +542,8 @@ onMounted(() => fetchList());
   }
 
   .total-count {
-    @include font-14-regular;
+    @include font-14-bold;
+
     color: $dark-text;
   }
 
@@ -561,10 +562,11 @@ onMounted(() => fetchList());
     }
 
     thead tr {
-      background: $main-gad;
+      background: $bg-op;
     }
     th {
       @include font-14-bold;
+      color: $gray;
     }
 
     th.sortable {
@@ -577,10 +579,6 @@ onMounted(() => fetchList());
         margin-left: 4px;
         font-size: 10px;
       }
-    }
-
-    tbody tr:nth-child(odd) {
-      background: $bg-op;
     }
 
     tbody tr.clickable-row {
